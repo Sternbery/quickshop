8b8ecf741c42af3cee10c21a5ef1493b3ff3a5e1
Stefan <stefansternberg@gmail.com>
2020-11-20 13:36:34 -0800

    first commit

---
+++ quickshop/src/test/java/com/collabera/letsgo/quickshop/SpringSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabera.letsgo.quickshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
